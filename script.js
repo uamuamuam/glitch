fe6468232a648e105f2d0cf12639a9ca06f4f172
Glitch (fire-clean-aletopelta) <none>
2020-03-05 18:53:33 +0000

    🚡🖐 Updated with Glitch

--- script.js
+++ script.js
@@ -4,96 +4,35 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-let canvasNumber = 0;
+let canvasNumber=0;
 
-let size = 4;
+let size=4
 function cambia() {
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
   let w = ctx.canvas.width;
   let h = ctx.canvas.height;
-  let sizep = document.getElementById("size");
-  sizep.innerHTML = `${size}`;
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, w, h);
-  draw();
-  canvasNumber = (canvasNumber + 1) % 2;
+  let sizep=document.getElementById("size")
+  sizep.innerHTML=`${size}`
+  ctx.fillStyle='white'
+ ctx.fillRect(0,0,w,h);
+  canvasNumber? draw(): draw2() ;
+  canvasNumber = (canvasNumber +1)%2
 }
 
-/*
+
+
+
 function draw() {
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
   let w = ctx.canvas.width;
   let h = ctx.canvas.height;
   console.log(w);
-  //  let size = 15;
+//  let size = 15;
   //  ctx.fillRect(10, 10, 200, 200);
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      ctx.strokeStyle = `rgb(${i * 10},${j * 10},30)`;
-      ctx.strokeRect((i * w) / size, (j * h) / size, w / size, h / size);
-      
-      ctx.beginPath();
-            ctx.strokeStyle = `rgb(${i*10},${j*10},i*j)`;
-
-
-      ctx.moveTo(
-        (i * w) / size + w / (2 * size),
-        (j * h) / size + h / (2 * size)
-      );
-      //  ctx.lineTo(200,200);
-      for (let k = 0; k < 6; k++) {
-        // ctx.lineTo(
-        // i * w/size + w / (2 * size) + ((Math.random() - 0.5) * w*1.3) / (size * 2),
-        //j * h/size + h / (2 * size) + ((Math.random() - 0.5) * h*1.3) / (size * 2)
-        //);
-        /*
-        ctx.quadraticCurveTo(
-          (i * w) / size +
-            w / (2 * size) +
-            ((Math.random() - 0.5) * w * 1.3) / (size * 2),
-          (j * h) / size +
-            h / (2 * size) +
-            ((Math.random() - 0.5) * h * 1.3) / (size * 2),
-          (i * w) / size + w / (2 * size) + ((Math.random() - 0.5) * w * 1.3)/(size*2),
-          (j * h) / size +
-            h / (2 * size) +
-            ((Math.random() - 0.5) * h * 1.3) / (size * 2)
-        );
-        
-        ctx.bezierCurveTo(
-          (i * w) / size +
-            w / (2 * size) +
-            ((Math.random() - 0.5) * w * 1.3) / (size * 2),
-          (j * h) / size +
-            h / (2 * size) +
-            ((Math.random() - 0.5) * h * 1.3) / (size * 2),
-          (i * w) / size +
-            w / (2 * size) +
-            ((Math.random() - 0.5) * w * 1.3) / (size * 2),
-          (j * h) / size +
-            h / (2 * size) +
-            ((Math.random() - 0.5) * h * 1.3) / (size * 2),
-          (i * w) / size +
-            w / (2 * size) +
-            ((Math.random() - 0.5) * w) / (size * 2),
-          (j * h) / size +
-            h / (2 * size) +
-            ((Math.random() - 0.5) * h) / (size * 2)
-        );
-        console.log("*");
-      }
-                  ctx.strokeStyle = `rgb(${i*20},${j*30},90)`;
-
-
-      ctx.stroke()
-
-
-      ctx.closePath();
-      ctx.strokeStyle = `rgb(${i*20},${j*30},90)`;
-     // console.log("linea", i, j);
-      /*     
       ctx.fillStyle = `rgb(${i * 20},${j * 30},90)`;
       //ctx.fillStyle='red'
       ctx.fillRect(
@@ -123,61 +62,40 @@ function draw() {
       //       ctx.arc(i*w/size,j*h/size,20,0,3,1)
          ctx.fill();
       ctx.closePath();
-      
+      */
     }
   }
 }
-*/
 
-size=12
-let time=0
-function draw() {
-  time++
+function draw2() {
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
   let w = ctx.canvas.width;
   let h = ctx.canvas.height;
- // console.log(w);
-  ctx.clearRect(0,0,w,h)
-  for (let i = 0; i < size; i++){
-    /*
-    ctx.save();
-    ctx.translate(w/2,h/2);
-    ctx.rotate(Math.PI*2/360*time)
-    ctx.rotate(Math.PI*2/size *i);
-    ctx.beginPath()
-    ctx.moveTo(0,0)
-    ctx.strokeStyle=`rgb(${i*30},${i*10},${i*30})`
-    ctx.lineTo(100-i*3,0)
-    ctx.stroke()
-    ctx.closePath()
-    ctx.beginPath()
-    ctx.arc(120-i*3,0,20,0, Math.PI * 2, true)
-    ctx.fillStyle=`rgb(${i*10},${i*20},${i*30})`
-    ctx.fill()
-    ctx.closePath()
-    ctx.restore()
-    */
-   ctx.save()
-    ctx.translate(w/2,h/2)
-    ctx.rotate(-2*Math.PI*2/360*time)
-    ctx.rotate(Math.PI*2/(2*size) *(2*i+1))
-   /* ctx.beginPath()
-    ctx.strokeStyle=`rgb(${i*30},${i*20},${i*30})`
-    ctx.moveTo(0,0)
-    ctx.lineTo(100-i*5,0)
-    ctx.stroke()
-    ctx.closePath()
-    */
-    ctx.fillStyle=`rgb(${i*10},${i*20},${i*10})`
-    ctx.translate(100-i*5,0)
-    ctx.rotate(Math.PI*2/size *i)
-    ctx.rotate(Math.PI*2/180*time)
-  //  ctx.fillRect(0,0,30,10*i)
-        ctx.fillRect(0,0,30,40)
-
-    ctx.restore()
+  /* console.log(w);*/
+//  let size = 2;
+  //  ctx.fillRect(10, 10, 200, 200);
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      
+      
+      ctx.strokeStyle = `rgb(${i * 20},${j * 30},8
+0)`;
+      ctx.strokeRect(i*w/size,j*h/size,w/size,h/size);
+      ctx.beginPath();
+      ctx.moveTo(i * w/size + w / (2 * size), j * h/size + h / (2 * size));
+    //  ctx.lineTo(200,200);
+      for (let k = 0; k < 10; k++) {
+        ctx.lineTo(
+          i * w/size + w / (2 * size) + ((Math.random() - 0.5) * w*1.3) / (size * 2),
+          j * h/size + h / (2 * size) + ((Math.random() - 0.5) * h*1.3) / (size * 2)
+        );
+        console.log('*')
+      }
+      ctx.stroke();
+      ctx.closePath();
+      console.log("linea",i,j)
+    }
   }
-  
 }
-setInterval( draw, 20);
+
